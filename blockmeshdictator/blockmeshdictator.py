b4aab5b5d9dad88fe504f2abcb238fe7ea79b940
hiaselhans <simon.klemenc@gmail.com>
2019-02-07 09:13:30 +0100

    latest

--- blockmeshdictator/blockmeshdictator.py
+++ blockmeshdictator/blockmeshdictator.py
@@ -4,16 +4,16 @@ import jinja2
 
 import scipy.optimize
 
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-env.filters.update({
-    "floatformat": lambda f: "{: 19.4f}".format(f),
-    "fourtimes": lambda s: "{0} {0} {0} {0}".format(s)
-})
 
 
 class BlockmeshDictator(object):
     whole_mesh = False
     inner_size = 0.1
+    bottom_cells = 10
+    top_cells = 10
+    side_cells = 8
+    inlet_cells = 15
+    outlet_cells = 15
 
     def __init__(self, inner_min, inner_max, outer_min, outer_max):
         self.inner_min = inner_min
@@ -21,11 +21,19 @@ class BlockmeshDictator(object):
         self.outer_min = outer_min
         self.outer_max = outer_max
 
+        self.env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+        self.env.filters.update({
+            "f": lambda f: "{:.4f}".format(f),
+            "f2": lambda f: "{: 10.4f}".format(f),
+            "fourtimes": lambda s: "{0} {0} {0} {0}".format(s)
+        })
+
     def get_grading(self, num_blocks, length_total):
         length_center = self.inner_size
         # L=sum(x^i*d_0, {i, 1, num_blocks}) -> sum(x^i) - L/d_0 == 0
         f = lambda x: sum([x ** (i+1) for i in range(num_blocks)]) - length_total / length_center
-        return scipy.optimize.newton(f, 1, maxiter=5000)**num_blocks
+        res = scipy.optimize.newton(f, 1, maxiter=5000)**num_blocks
+        return res
 
     @staticmethod
     def get_length(num_blocks, length_center, grading):
@@ -38,15 +46,15 @@ class BlockmeshDictator(object):
         outer_max = self.outer_max
         outer_min = self.outer_min
 
-        self.vertices = {
-            # min -> max
-            # AA--BA--CA
-            # |   |   |
-            # AB--BB--CB
-            # |   |   |
-            # AC--BC--CC
-            # |   |   |
-            # AD--BD--CD
+        self.vertices = {#  CBD--DBD
+            # Xmin -> max  /    /|
+            # AAD--BAD--CAD--DAD DBC
+            # |    |    |  / |  /|
+            # AAC--BAC--CAC--DAC DBB
+            # |    |    |  / |  /|
+            # AAB--BAB--CAB--DAB DBA
+            # |    |    |  / |  /
+            # AAA--BAA--CAA--DAA
 
             "AAA": (outer_min[0], outer_min[1], outer_min[2]),
             "AAB": (outer_min[0], outer_min[1], inner_min[2]),
@@ -133,13 +141,25 @@ class BlockmeshDictator(object):
 
 
     def get_blockmeshdict(self):
+        context = {
+            "inner_size": self.inner_size,
+            "inner_min": self.inner_min,
+            "inner_max": self.inner_max,
+            "outer_min": self.outer_min,
+            "outer_max": self.outer_max,
+            "whole_mesh": self.whole_mesh,
+            "bottom_cells": self.bottom_cells,
+            "top_cells": self.top_cells,
+            "side_cells": self.side_cells,
+            "inlet_cells": self.inlet_cells,
+            "outlet_cells": self.outlet_cells
+        }
+
         if not self.whole_mesh:
             # TODO: Set inner_max and outer_max y 0
-            #mesh_params["inner_max"][1] = 0
-            #mesh_params["outer_max"][1] = 0
-            pass
+            context["inner_min"][1] = 0
+            context["outer_min"][1] = 0
 
-        context = {}
 
         # Vertice-koordinaten berechnen:
         def get_num(i):
@@ -149,8 +169,6 @@ class BlockmeshDictator(object):
         context["inner_num_y"] = get_num(1)
         context["inner_num_z"] = get_num(2)
 
-        inner_size = context["inner_size"]
-
         context["bottom_grading"] = self.get_grading(context["bottom_cells"], self.inner_min[2] - self.outer_min[2])
         context["top_grading"] = 1/self.get_grading(context["top_cells"], self.outer_max[2] - self.inner_max[2])
 
@@ -158,150 +176,33 @@ class BlockmeshDictator(object):
         context["outlet_grading"] = 1/self.get_grading(context["outlet_cells"], self.outer_max[0] - self.inner_max[0])
 
         context["side_grading"] = self.get_grading(context["side_cells"], self.outer_max[1] - self.inner_max[1])
+
         if self.whole_mesh:
             context["side_grading_inv"] = 1/self.get_grading(context["side_cells"], self.inner_min[1] - self.outer_min[1])
 
+        ids = {}
+        vertices = []
+        for i, (key, vertex) in enumerate(self.get_vertices().items()):
+            ids[key] = i
+            vertices.append(vertex)
+
+        def get_vertex_ids(string: str):
+            vertice_names = string.split(" ")
+            vertice_ids = [str(ids[key]) for key in vertice_names]
+            return " ".join(vertice_ids)
+
+        self.env.filters.update({
+            "node_ids": get_vertex_ids
+        })
 
 
-        return env.get_template("blockMeshDict").render(vertices=vertices, **mesh_params)
+        return self.env.get_template("new").render(vertices=vertices, **context)
 
+inner_max = [1, 5, 1]
+inner_min = [0, -5, -8]
 
+outer_max = [10, 8, 10]
+outer_min = [-8, -8, -8]
 
-def get_vertices(mesh_params):
-    inner_max = mesh_params["inner_max"]
-    inner_min = mesh_params["inner_min"]
-    outer_max = mesh_params["outer_max"]
-    outer_min = mesh_params["outer_min"]
-
-
-    vertices2 = [
-        # Center block
-        [inner_max[0], inner_min[1], inner_max[2]],  # V_0
-        [inner_min[0], inner_min[1], inner_max[2]],  # V_1
-        [inner_min[0], inner_max[1], inner_max[2]],  # V_2
-        inner_max,  # V_3
-        [inner_max[0], inner_min[1], inner_min[2]],  # V_4
-        inner_min,  # V_5
-        [inner_min[0], inner_max[1], inner_min[2]],  # V_6
-        [inner_max[0], inner_max[1], inner_min[2]],  # V_7
-
-        # center Bottom (+z richtung)
-        [inner_max[0], inner_min[1], outer_max[2]],  # V_8
-        [inner_min[0], inner_min[1], outer_max[2]],  # V_9
-        [inner_min[0], inner_max[1], outer_max[2]],  # V_10
-        [inner_max[0], inner_max[1], outer_max[2]],  # V_11
-
-        # center Top (-z richtung)
-        [inner_max[0], inner_min[1], outer_min[2]],  # V_12
-        [inner_min[0], inner_min[1], outer_min[2]],  # V_13
-        [inner_min[0], inner_max[1], outer_min[2]],  # V_14
-        [inner_max[0], inner_max[1], outer_min[2]],  # V_15
-
-        # center left (-y richtung)
-        [inner_max[0], outer_min[1], inner_max[2]],  # V_16
-        [inner_min[0], outer_min[1], inner_max[2]],  # V_17
-        [inner_min[0], outer_min[1], inner_min[2]],  # V_18
-        [inner_max[0], outer_min[1], inner_min[2]],  # V_19
-
-        # center edge left bottom:
-        [inner_max[0], outer_min[1], outer_max[2]],  # V_20
-        [inner_min[0], outer_min[1], outer_max[2]],  # V_21
-
-        # center edge left top:
-        [inner_max[0], outer_min[1], outer_min[2]],  # V_22
-        [inner_min[0], outer_min[1], outer_min[2]],  # V_23
-
-        # center right block (+y richtung)
-        [inner_max[0], outer_max[1], inner_max[2]],  # V_24
-        [inner_min[0], outer_max[1], inner_max[2]],  # V_25
-        [inner_max[0], outer_max[1], inner_min[2]],  # V_26
-        [inner_min[0], outer_max[1], inner_min[2]],  # V_27
-
-        # center edge right bottom:
-        [inner_max[0], outer_max[1], outer_max[2]],  # V_28
-        [inner_min[0], outer_max[1], outer_max[2]],  # V_29
-
-        # center edge right top:
-        [inner_max[0], outer_max[1], outer_min[2]],  # V_30
-        [inner_min[0], outer_max[1], outer_min[2]],  # V_31
-
-        # outlet center block
-        [outer_min[0], inner_max[1], inner_min[2]],  # V_32
-        [outer_min[0], inner_max[1], inner_max[2]],  # V_33
-        [outer_min[0], inner_min[1], inner_max[2]],  # V_34
-        [outer_min[0], inner_min[1], inner_min[2]],  # V_35
-
-        # inlet center block
-        [outer_max[0], inner_max[1], inner_min[2]],  # V_36
-        [outer_max[0], inner_max[1], inner_max[2]],  # V_37
-        [outer_max[0], inner_min[1], inner_min[2]],  # V_38
-        [outer_max[0], inner_min[1], inner_max[2]],  # V_39
-
-        # outlet plane
-        [outer_min[0], outer_max[1], outer_min[2]],  # V_40
-        [outer_min[0], outer_max[1], inner_min[2]],  # V_41
-        [outer_min[0], outer_max[1], inner_max[2]],  # V_42
-        [outer_min[0], outer_max[1], outer_max[2]],  # V_43
-        outer_min,  # V_44
-        [outer_min[0], outer_min[1], inner_min[2]],  # V_45
-        [outer_min[0], outer_min[1], inner_max[2]],  # V_46
-        [outer_min[0], outer_min[1], outer_max[2]],  # V_47
-        [outer_min[0], inner_max[1], outer_min[2]],  # V_48
-        [outer_min[0], inner_min[1], outer_min[2]],  # V_49
-        [outer_min[0], inner_max[1], outer_max[2]],  # V_50
-        [outer_min[0], inner_min[1], outer_max[2]],  # V_51
-
-        # inlet plane
-        [outer_max[0], outer_max[1], outer_min[2]],  # V_52
-        [outer_max[0], outer_max[1], inner_min[2]],  # V_53
-        [outer_max[0], outer_max[1], inner_max[2]],  # V_54
-        outer_max,  # V_55
-        [outer_max[0], outer_min[1], outer_min[2]],  # V_56
-        [outer_max[0], outer_min[1], inner_min[2]],  # V_57
-        [outer_max[0], outer_min[1], inner_max[2]],  # V_58
-        [outer_max[0], outer_min[1], outer_max[2]],  # V_59
-        [outer_max[0], inner_max[1], outer_min[2]],  # V_60
-        [outer_max[0], inner_min[1], outer_min[2]],  # V_61
-        [outer_max[0], inner_max[1], outer_max[2]],  # V_62
-        [outer_max[0], inner_min[1], outer_max[2]]  # V_63
-    ]
-
-    return vertices
-
-
-def return_blockmeshdict(mesh_params):
-    if not mesh_params["whole_mesh"]:
-        mesh_params["inner_max"][1] = 0
-        mesh_params["outer_max"][1] = 0
-
-    # Vertice-koordinaten berechnen:
-    def get_num(i):
-        return int((mesh_params["inner_max"][i] - mesh_params["inner_min"][i]) // mesh_params["inner_size"])
-
-    mesh_params["inner_num_x"] = get_num(0)
-    mesh_params["inner_num_y"] = get_num(1)
-    mesh_params["inner_num_z"] = get_num(2)
-
-    def auto_grading(keyword_grading, keyword_number, length, inverse=False):
-        if mesh_params[keyword_grading] is None and mesh_params[keyword_number] is None:
-            raise ValueError("either %s or %s have to be set for auto-grading" % (keyword_grading, keyword_number))
-        elif mesh_params[keyword_grading] is None:
-            if inverse:
-                #print(mesh_params[keyword_number], length)
-                mesh_params[keyword_grading] = 1/get_grading(mesh_params[keyword_number], mesh_params["inner_size"], abs(length))
-            else:
-                mesh_params[keyword_grading] = get_grading(mesh_params[keyword_number], mesh_params["inner_size"], abs(length))
-        else:
-            raise NotImplementedError("can only auto-grade not auto-cellno")
-
-    auto_grading("bottom_grading", "bottom_cells", mesh_params["outer_max"][2] - mesh_params["inner_max"][2])
-    auto_grading("top_grading", "top_cells", mesh_params["inner_max"][2] - mesh_params["outer_max"][2], inverse=True)
-
-    auto_grading("inlet_grading", "inlet_cells", mesh_params["inner_min"][0] - mesh_params["outer_min"][0])
-    auto_grading("outlet_grading", "outlet_cells", mesh_params["inner_min"][0] - mesh_params["outer_min"][0], inverse=True)
-    auto_grading("side_grading", "side_cells", mesh_params["inner_min"][1] - mesh_params["outer_min"][1], inverse=True)
-    mesh_params["side_grading_inv"] = 1/mesh_params["side_grading"]
-
-    vertices = get_vertices(mesh_params)
-
-    return env.get_template("blockMeshDict").render(vertices=vertices, **mesh_params)
+a = BlockmeshDictator(inner_min, inner_max, outer_min, outer_max)
+print(a.get_blockmeshdict())
